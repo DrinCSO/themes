b4f11ea7007c6eb47279d44dba9ecba0f40c0e27
DrinCSO <dkryeziu464@gmail.com>
2025-03-06 19:12:23 +0100

    ds_themes_final

---
+++ ds_themes_final/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html <?php language_attributes()?>>
+<head>
+    <meta charset="<?php bloginfo('charset')?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque, animi.</p>
+    <header>
+        <section class="top_bar">
+            <div class="logo">
+                Logo
+            </div>
+            <div class="searchbox">
+                Search
+            </div>
+        </section>
+        <section class="menu-area">
+            <nav class="main-menu">
+                Menu
+            </nav>
+
+        </section>
+    </header>
