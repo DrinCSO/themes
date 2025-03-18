b5b29d909db9484a94ad6267ac3af69376800b54
DrinCSO <dkryeziu464@gmail.com>
2025-03-18 19:22:36 +0100

    update

---
+++ ds_themes_final/js/dropdown.js
@@ -0,0 +1,4 @@
+let dropdown = document.querySelector('.menu'), //ul
+submeny = document.querySelector('.sub-menu'),//ul li a
+buttonclick = document.querySelector('.check-button'),//button
+habmburger =  document.querySelector('.menu-icon'); 
